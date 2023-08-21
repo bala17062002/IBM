40c5a69f1346b94b8dfffe0e4d54296a88d1ee2f
balu <kbalaji2220@gmail.com>
2023-08-22 00:51:01 +0530

    fourth push by balu

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle
 import numpy as np
 
 model = pickle.load(open('predection.pkl','rb'))
+model1 = pickle.load(open('salary.pkl','rb'))
 
 app=Flask(__name__)
 
@@ -12,10 +13,16 @@ def welcome():
 @app.route('/select')
 def select():
     return render_template('select_clg.html')
+@app.route('/trend')
+def trend():
+    return render_template('Clgtrend.html')
 @app.route('/reg')
 def reg():
     reg=1
     return redirect(url_for('login'))
+@app.route('/result')
+def result():
+    return render_template('results.html')
 @app.route('/data')
 def data():
      data1 = 1
@@ -27,10 +34,12 @@ def data():
      data7 = 1
      data8 = 1
      data9 = 75
-     data10 = 1
+     data10 = 0
      arr1 = np.array([[data1,data2,data3,data4,data5,data6,data7,data8,data9,data10]])
      data11=model.predict(arr1)
-     return render_template('result.html',data=data11)
-
+     d2=data11[0]
+     arr2 = np.array([[data1,data2,data3,data4,data5,data6,data7,data8,data9,data10,0]])
+     salary=model1.predict(arr2)
+     return render_template('results.html',data=salary)
 if __name__=='__main__':
     app.run(debug=True)
